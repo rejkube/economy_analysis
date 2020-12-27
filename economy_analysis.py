1460ef245372e4bc0ef36828dfbf27377c922c13
rejkube <rejithrr@protonmail.com>
2020-12-27 16:10:57 +0530

    release0.3

--- economy_analysis.py
+++ economy_analysis.py
@@ -10,7 +10,7 @@ import seaborn as sns
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title("CountryForeignExchangeReserves")
+st.title("WorldBank Forex Reserve")
 
 df_data=pd.read_csv('COFER_12-27-2020 06-41-57-24.csv')
 df_data=df_data.drop(columns=['Status','Unnamed: 7'],axis=1)
@@ -25,16 +25,26 @@ World_Economy[['Time Period','Value','Reserve_Type','Detail_Reserve']].sort_valu
 currency_reserve=World_Economy[World_Economy.Reserve_Type.isin(['Allocated Reserves'])
                                & World_Economy.Detail_Reserve.str.contains('US Dollars')]
 currency_reserve=currency_reserve[['Time Period','Value','Detail_Reserve']].sort_values(['Time Period'],ascending=[True])
-currency_reserve['Value']=(currency_reserve['Value']/100000).round(2)
+currency_reserve['Value']=(currency_reserve['Value']/(10**12))
 currency_reserve=currency_reserve.set_index('Time Period')
 #Plot begin
 st.table(currency_reserve)
-st.header("World Economy Distribution in USD")
+st.header("World Bank Reserve in USD(Trillions)")
 st.subheader("Bar Plot")
 currency_reserve.plot(kind='bar')
 st.pyplot()
 
 st.subheader("Displot")
-sns.displot(currency_reserve['Value'])
+sns.displot(x='Time Period',y='Value',data=currency_reserve)
+st.pyplot()
+
+#Scatter_joint
+st.subheader("ScatterPlot")
+sns.jointplot(x='Time Period',y='Value',data=currency_reserve,kind='scatter')
+st.pyplot()
+
+##Rugplot
+st.subheader("Rugplot")
+sns.rugplot(x='Time Period',y='Value',data=currency_reserve)
 st.pyplot()
 
